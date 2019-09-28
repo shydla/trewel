5cd611227ea6e55379f132d82f74f12ded51cb13
shydla <shydla@gmail.com>
2019-09-28 09:59:09 +0300

    add cities and form

---
+++ cities/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+
+class HtmlForm(forms.Form):
+    name = forms.CharField(label='Город')
--- cities/views.py
+++ cities/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from .models import City
+from .forms import HtmlForm
 
 
 def home(request):
+    if request.method == 'POST':
+        form = HtmlForm(request.POST or None)
+    # print(request.POST)
     cities = City.objects.all()
-    return render(request, 'cities/home.html', {'objects_list': cities})
+    return render(request, 'cities/home.html', {'objects_list': cities, 'form': form})
+
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    context_object_name = 'object'
+    template_name = 'cities/detail.html'
