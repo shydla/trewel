731cca3bf2d42d6976c14a129311d874652dc988
shydla <shydla@gmail.com>
2019-10-04 23:01:50 +0300

    add cities and form1

--- cities/urls.py
+++ cities/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, CityDetailView
+from .views import home, CityDetailView, CityCreateView, CityDeleteView, CityUpdateView
 
 urlpatterns = [
     path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
+    path('add/', CityCreateView.as_view(), name='add'),
+    path('update/', CityUpdateView.as_view(), name='update'),
+    path('delete/', CityDeleteView.as_view(), name='delete'),
     path('', home, name='home'),
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import City
-from .forms import HtmlForm
+from .forms import CityForm
+from django.urls import reverse_lazy
 
 
 def home(request):
     if request.method == 'POST':
-        form = HtmlForm(request.POST or None)
+        form = CityForm(request.POST or None)
+        if form.is_valid():
+            print(form.cleaned_data)
+    form = CityForm()
+    city = request.POST.get('name')
     # print(request.POST)
     cities = City.objects.all()
     return render(request, 'cities/home.html', {'objects_list': cities, 'form': form})
@@ -16,3 +22,22 @@ class CityDetailView(DetailView):
     queryset = City.objects.all()
     context_object_name = 'object'
     template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('city:home')
+
+class CityDeleteView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/delete.html'
+    success_url = reverse_lazy('city:home')
+
+class CityUpdateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('city:home')
